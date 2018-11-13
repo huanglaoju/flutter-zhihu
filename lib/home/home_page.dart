bcd8658b7f159f9d55f5416a95c4e427be29baf4
huangju <993808954@qq.com>
2018-11-13 14:13:04 +0800

    完善首页

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import '../global_config.dart';
+import 'follow.dart';
+import 'recommend.dart';
+import 'hot.dart';
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -10,32 +13,50 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Widget barSearch() {
     return new Container(
-      child: new Row(
-        children: <Widget>[
-          new Expanded(
-            child: new FlatButton.icon(
-              onPressed: () {},
-              icon: new Icon(
-                Icons.search,
-                size: 16.0,
+        child: new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new FlatButton.icon(
+                onPressed: () {},
+                icon: new Icon(
+                  Icons.search,
+                  size: 16.0,
+                  color: GlobalConfig.fontColor,
+                ),
+                label: new Text(
+                  'IG牛逼',
+                  style: new TextStyle(color: GlobalConfig.fontColor),
+                ),
               ),
-              label: new Text('坚果手机'),
             ),
-          ),
-          new Container(
-            height: 14.0,
-            width: 1.0,
-          ),
-          new Container(
-            child: new FlatButton.icon(
-              onPressed: () {},
-              icon: new Icon(Icons.border_color),
-              label: new Text('提问'),
+            new Container(
+              decoration: new BoxDecoration(
+                  border: new BorderDirectional(
+                      start: new BorderSide(
+                          color: GlobalConfig.fontColor, width: 1.0))),
+              height: 14.0,
+              width: 1.0,
             ),
-          )
-        ],
-      ),
-    );
+            new Container(
+              child: new FlatButton.icon(
+                onPressed: () {},
+                icon: new Icon(
+                  Icons.border_color,
+                  color: GlobalConfig.fontColor,
+                  size: 14.0,
+                ),
+                label: new Text(
+                  '提问',
+                  style: new TextStyle(color: GlobalConfig.fontColor),
+                ),
+              ),
+            )
+          ],
+        ),
+        decoration: new BoxDecoration(
+          borderRadius: const BorderRadius.all(const Radius.circular(4.0)),
+          color: GlobalConfig.searchBackgroundColor,
+        ));
   }
 
   @override
@@ -46,8 +67,11 @@ class _HomePageState extends State<HomePage> {
         appBar: new AppBar(
           title: barSearch(),
           bottom: new TabBar(
-            labelColor: Colors.blue,
-            unselectedLabelColor: Colors.black,
+            isScrollable: true,
+            labelColor:
+                GlobalConfig.dark == true ? new Color(0xFF63FDD9) : Colors.blue,
+            unselectedLabelColor:
+                GlobalConfig.dark == true ? Colors.white : Colors.black,
             tabs: <Widget>[
               new Tab(text: '关注'),
               new Tab(text: '推荐'),
@@ -57,7 +81,9 @@ class _HomePageState extends State<HomePage> {
         ),
         body: new TabBarView(
           children: <Widget>[
-            // new Follow(),
+            new Follow(),
+            new Recommend(),
+            new Hot()
           ],
         ),
       ),
